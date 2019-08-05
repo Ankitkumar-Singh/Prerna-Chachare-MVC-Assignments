ae14aa34b534bcd8d9b7684c1898cf90fe48cc6f
Prerna Chachare <prerna.chachare@aressindia.net>
2019-08-05 11:53:05 +0530

    Project Updated

---
+++ Assignment/Assignment/Controllers/Assignment4Controller.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Assignment.Controllers
+{
+    public class Assignment4Controller : Controller
+    {
+        /// <summary>
+        /// View to display user details using ADO .NET
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            EmployeeBusinessLayer employeeBusinessLayer =
+                new EmployeeBusinessLayer();
+
+            List<Employee> employees = employeeBusinessLayer.Employees.ToList();
+            return View(employees);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
